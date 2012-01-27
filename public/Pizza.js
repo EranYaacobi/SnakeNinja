1e4365959ff60f2c072ffb377ac2339133d1f3d2
eranyaacobi <eran.yaacobi@gmail.com>
2012-01-27 18:01:12 +0000

    4

--- public/Pizza.js
+++ public/Pizza.js
@@ -1,28 +1,28 @@
-var Pizza = function(game)
+SnakeNinja.Pizza = function(game)
 {
+    this.Game = game;
+    
     this.Spawn = function (position, size, time)
     {
         this.Size = size || 1;
         this.Position = position;
         this.TimeSpan = time || 20000;
-        //this.Image = "images/apple" + size + ".png";
-    }
+    };
     
     this.Draw = function(graphics)
     {
-        var img = game.Resources["imgPizza"];
+        var img = this.Game.Resources["imgPizza"];
         graphics.drawImage(img, this.Position.X - img.width/2  , this.Position.Y - img.height/2);
-    }
+    };
     
     this.Update = function(ts)
     {
         
-    }
-    
+    };
     
     this.Eaten = function (snake)
     {
         snake.Increase(this.Size);
-    }
+    };
     
-}
+};
