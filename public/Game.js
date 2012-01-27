4d5d7a892d144e9cb400acd9cf3db2b4ee247849
Ehnn <adamzeira@gmail.com>
2012-01-27 15:27:13 +0200

    1

--- public/Game.js
+++ public/Game.js
@@ -6,3 +6,26 @@ jQuery(function()
             source: "images/fish.jpg",
             x: 150, y: 150
     });
+
+var UPDATES_PER_SECOND = 30;
+var UPDATE_TIME = 1000 / UPDATES_PER_SECOND;
+
+var Game = function () {
+	var that = this;
+	var gameLoop = null;
+
+	this.Init = function () {
+		gameLoop = setIntevral(Loop, UPDATE_TIME);
+	};
+
+	var Loop = function () {
+		that.Update();
+		that.Draw();
+	};
+	
+	this.Update = function () {
+	};	
+	
+	this.Draw = function () {
+	};
+};
