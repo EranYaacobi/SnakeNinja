ee763abae2dce4fd4ee590974dfd06554052a69f
XCaster <yshayy@gmail.com>
2012-01-27 15:43:04 +0000

    try

--- public/Apple.js
+++ public/Apple.js
@@ -1,5 +1,28 @@
-var Apple = function()
+var Apple = function(game)
 {
-    this.x;
-    this.y
+    this.Spawn = function (position, size, time)
+    {
+        this.Size = size || 1;
+        this.Position = position;
+        this.TimeSpan = time || 20000;
+        this.Image = "images/apple.png";
+    }
+    
+    
+    
+    this.Draw = function(graphics)
+    {
+        graphics.drawImage(
+    }
+    
+    this.Update = function()
+    {
+    }
+    
+    
+    this.Eaten = function (snake)
+    {
+        snake.length
+    }
+    
 }
