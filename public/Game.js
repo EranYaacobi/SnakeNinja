b26774619810121e32f4346edd807c3ef3d93ae2
eranyaacobi <eran.yaacobi@gmail.com>
2012-01-27 11:52:18 +0000

    third commit

---
+++ public/Game.js
@@ -0,0 +1,8 @@
+jQuery(function()
+    {
+        var canvas = jQuery("#GameCanvas");
+    
+        $("canvas").drawImage({
+            source: "images/fish.jpg",
+            x: 150, y: 150
+    });
