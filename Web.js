a20c929e36f48094b82d07b4132cadba69a7c973
adamzeira <adamzeira@gmail.com>
2012-01-28 13:16:49 +0000

    server-client data transfer

--- Web.js
+++ Web.js
@@ -1,14 +1,69 @@
+var SENDS_PER_SECOND = 1;
+var SEND_INTERVAL = 1000 / SENDS_PER_SECOND;
+
 var express = require('express');
 var app = express.createServer(
-    
     express.static(__dirname + '/public')
 );
 
-
 io = require('socket.io').listen(app);
 
-app.get("/", function (req,res)
-{
+app.get("/", function (req,res) {
     res.redirect("/Game.html");
 });
+
 app.listen(process.env.PORT);
+
+var Sockets = [];
+var PlayerCount = 0;
+var Snakes = [];
+
+var broadcast = function (event, data1, data2, data3) {
+    for (var i in Sockets)
+	{
+		var socket = Sockets[i];
+		socket.emit(event, data1, data2, data3);
+	}
+};
+
+var SendData = function () {
+    broadcast('serverdata', Snakes);
+};
+
+var FindSnakeIndex = function (ID) {
+    for (var i in Snakes) {
+        var snake = Snakes[i];
+        if (snake.ID == ID)
+            return i;
+    }
+    return -1;
+};
+
+var SendLoop = setInterval(SendData, SEND_INTERVAL);
+
+console.log('ready for connections');
+
+io.sockets.on('connection', function (socket) {
+    console.log("player connected!");
+    socket.PlayerID = PlayerCount++;
+    Sockets.push(socket);
+    
+    socket.emit('joinconfirmed', socket.PlayerID);
+    
+    socket.on('disconnect', function () {
+        var index = FindSnakeIndex(socket.PlayerID);
+        Snakes.splice(index, 1);
+		for (var i in Sockets)
+			if (Sockets[i].PlayerID == socket.PlayerID)
+				Sockets.splice(i, 1);
+	});
+    
+    socket.on('playerdata', function (playerdata) {
+        var index = FindSnakeIndex(playerdata.ID);
+        if (index == -1)
+            /** New player */
+            Snakes.push(playerdata);
+        else
+            Snakes[index] = playerdata;
+    });
+});
--- public/Game.js
+++ public/Game.js
@@ -2,9 +2,13 @@ SnakeNinja = {};
 
 var UPDATES_PER_SECOND = 30;
 var UPDATE_TIME = 1000 / UPDATES_PER_SECOND;
+var SEND_DATA_PER_SECOND = 1;
+var SEND_DATA_TIME = 1000 / SEND_DATA_PER_SECOND;
 
 var CANVAS_WIDTH = 800;
 var CANVAS_HEIGHT = 600;
+
+var SERVER_CONNECTION = "localhost";
 
 var resourcestoload = {
     /*"sndEat" : {
@@ -32,6 +36,7 @@ var resourcestoload = {
 SnakeNinja.Game = function () {
 	var that = this;
 	var gameLoop = null;
+    var sendDataLoop = null;
 
     this.canvas = null;
     this.context2D = null;
@@ -48,6 +53,7 @@ SnakeNinja.Game = function () {
     this.Resources = {};
     
     var started = false;
+    var socket;
     
     /** load images and sound */
     var loadResources = function (callback) {
@@ -109,7 +115,31 @@ SnakeNinja.Game = function () {
         });
 	};
     
+    var Connect = function () {
+        socket = io.connect();
+        socket.emit('join', {});
+        socket.on('serverdata', ReceiveData);
+        socket.on('joinconfirmed', function (id) {
+            alert("id assigned:" + id);
+            sendDataLoop = setInterval(SendData, SEND_DATA_TIME);
+        });
+    };
+    
+    var ReceiveData = function (snakes) {
+        /** */
+        alert('received data');
+        /*for (var i in that.Snakes) {
+            var snake = that.Snakes[i];
+            that.mySnake.UpdateData();
+        }*/
+    };
+
+    var SendData = function () {
+        socket.emit('playerdata', { ID: 5/*that.mySnake.ID*/, Something:5 });
+    };
+    
     this.Start = function () {
+        Connect();
         started = true;
         lastFrame = new Date().getTime();
         that.mySnake = new SnakeNinja.Snake(this);
